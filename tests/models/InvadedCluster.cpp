e2429a5ea1ceb5e4eb5269354b2412f6b5456c06
apizzimenti <apizzime@gmu.edu>
2026-06-19 18:38:29 -0400

    add tests

---
+++ tests/models/InvadedCluster.cpp
@@ -0,0 +1,37 @@
+
+#include <ATEAMS++/ATEAMS++.h>
+#include <ATEAMS++/models/InvadedCluster.h>
+#include <ATEAMS++/complexes/Cubical.h>
+#include <ATEAMS++/arithmetic/persistence.h>
+#include <ATEAMS++/arithmetic/options.h>
+
+using namespace ATEAMS;
+using namespace std;
+
+int main() {
+	vector<int> corners = {3,3,3,3};
+	complexes::Cubical C(corners, true);
+
+	models::InvadedClusterParameters params;
+	params.field = 3;
+	params.stoppingFunction = arithmetic::stopInvadingAt({3,4});
+	params.dimension = 2;
+	params.DEBUG = false;
+
+	models::InvadedCluster SW(&C, params);
+	SW.initialize();
+
+	ATEAMS::arithmetic::ThreadOptions options;
+	std::thread listener = options.spinUp();
+
+	int N = 100;
+
+	for (int t=0; t < N; t++) {
+		SW.sample(t, options);
+	}
+
+	options.spinDown(&listener);
+	
+	return 0;
+}
+
