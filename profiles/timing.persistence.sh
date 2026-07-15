b2e8cbaa23b4ab0a5a876bc1ef33d89eeccae743
apizzimenti <apizzime@gmu.edu>
2026-07-15 09:17:30 -0400

    reduce fields + attempts + change name

--- profiles/timing.persistence.sh
+++ profiles/timing.persistence.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 SCALES=(3 4 6 8 11 16 23 32 45 64)
-FIELDS=(2 3 5)
+FIELDS=(2 3)
 DIMENSIONS=(1 2)
-ATTEMPTS=1000
+ATTEMPTS=100
 EXEC="persistence"
 
 mkdir -p ./profiles/timing
@@ -12,8 +12,8 @@ for SCALE in "${SCALES[@]}"; do
 	for DIMENSION in "${DIMENSIONS[@]}"; do
 		for FIELD in "${FIELDS[@]}"; do
 			PADDED=${(l(2)(0))SCALE}
-			PREFIX="$EXEC.$SCALE.$DIMENSION.$FIELD.$ATTEMPTS"
-			
+			PREFIX="$EXEC.$PADDED.$DIMENSION.$FIELD.$ATTEMPTS"
+
 			echo $PREFIX
 			./build/timing.$EXEC $SCALE $DIMENSION $FIELD $ATTEMPTS >> ./profiles/timing/$PREFIX.data
 		done
